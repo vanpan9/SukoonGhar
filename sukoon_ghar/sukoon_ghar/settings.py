0cbacb070740a1d37379f500c762ad7f2cf29e77
Vanshika Panwar <vanshikapanwar02906@gmail.com>
2024-01-28 02:53:32 +0530

    fix: Second Commit

--- sukoon_ghar/sukoon_ghar/settings.py
+++ sukoon_ghar/sukoon_ghar/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'sukoon_ghar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'sukoon',
-        'USER':'postgres',
-        'PASSWORD':'4421',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':BASE_DIR/'db.sqlite3'
     }
 }
 
